1e979cb623c00a32808021f89119b74f0fd9efe6
saini3080 <saini.gagan.3080@gmail.com>
2018-08-09 15:20:36 +0530

    setup

---
+++ app/Http/Controllers/admin/testController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class testController extends Controller
+{
+    //
+    public function test2($value='')
+    {
+    	# code...
+    	return "yess";
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    const SUPER_USER = 1;
+    const USER_AGENT = 2;
+    const USER_BUYER = 3;
+    const USER_DISABLED = 0;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    public function isAdmin()    {        
+        return $this->role === self::SUPER_USER;    
+    }
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
