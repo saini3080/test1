cc737ba7492907b5cf65333b080584a0a2253286
saini3080 <saini.gagan.3080@gmail.com>
2018-08-10 10:52:50 +0530

    making admin layout

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,36 @@
+<!-- Scripts -->
+
+<!-- <script src="{{ asset('js/app.js') }}"></script> -->
+
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/jquery/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/jquery/jquery-ui.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/bootstrap/bootstrap.min.js') }}"></script>
+
+         
+    
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/icheck/icheck.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/scrolltotop/scrolltopcontrol.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/morris/raphael-min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/morris/morris.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/rickshaw/d3.v3.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/rickshaw/rickshaw.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/bootstrap/bootstrap-datepicker.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/owl/owl.carousel.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/moment.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins/daterangepicker/daterangepicker.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('admin_asset/js/settings.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/plugins.js') }}"></script>
+<script type="text/javascript" src="{{ asset('admin_asset/js/actions.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset('admin_asset/js/demo_dashboard.js') }}"></script>
+
+</body> 
+</html>
